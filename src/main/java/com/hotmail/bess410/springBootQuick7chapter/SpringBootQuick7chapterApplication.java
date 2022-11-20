f1ba121f3ba94d19cb151f91920f0d6504dc163b
Andrey Sterkhov <asterkhov@65apps.com>
2022-11-20 18:42:14 +0400

    initial commit

---
+++ src/main/java/com/hotmail/bess410/springBootQuick7chapter/SpringBootQuick7chapterApplication.java
@@ -0,0 +1,13 @@
+package com.hotmail.bess410.springBootQuick7chapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuick7chapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuick7chapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotmail/bess410/springBootQuick7chapter/SpringBootQuick7chapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotmail.bess410.springBootQuick7chapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuick7chapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
