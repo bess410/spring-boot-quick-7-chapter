3cc1e72edf92d7e5590deba65b4e22eaabc06558
Andrey Sterkhov <asterkhov@65apps.com>
2022-12-04 18:59:12 +0400

    7 chapter using websocket to get data

--- src/main/java/com/hotmail/bess410/springBootQuick7chapter/PositionRetriever.java
+++ src/main/java/com/hotmail/bess410/springBootQuick7chapter/PositionRetriever.java
@@ -5,7 +5,11 @@ import com.hotmail.bess410.springBootQuick7chapter.repository.AircraftRepository
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
+import java.lang.reflect.InaccessibleObjectException;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -13,13 +17,26 @@ import java.util.function.Consumer;
 @Configuration
 public class PositionRetriever {
     private final AircraftRepository repo;
+    private final WebSocketHandler handler;
 
     @Bean
     Consumer<List<Aircraft>> retrieveAircraftPositions() {
         return acList -> {
             repo.deleteAll();
             repo.saveAll(acList);
-            repo.findAll().forEach(System.out::println);
+            sendPositions();
         };
+    }
+
+    private void sendPositions() {
+        if (repo.count() > 0) {
+            for (WebSocketSession sessionInList : handler.getSessionList()) {
+                try {
+                    sessionInList.sendMessage(new TextMessage(repo.findAll().toString()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
